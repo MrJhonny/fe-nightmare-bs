ecbf26bfe2ea4570db135e5f0fbf742328d27014
Jhon Müller <jhonvidalb@gmail.com>
2024-12-22 01:44:18 -0300

    https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ModalForm from "./Components/ModalForm";
 import FloatingButton from "./Components/FloatingButton";
 
 function App() {
-  const API_BASE_URL = process.env.REACT_APP_API_URL || "https://api.tellmeyournightmare.com:3000/api/v1/";
+  const API_BASE_URL = process.env.REACT_APP_API_URL || "https://api.tellmeyournightmare.com/api/v1/";
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
