d58a7bfe421ef45881e51e424bc367a7609c5cd6
Jhon Müller <jhonvidalb@gmail.com>
2024-12-22 21:09:12 -0300

    gtag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Components/Header";
 import AppRoutes from "./Components/Routes";
 import ModalForm from "./Components/ModalForm";
 import FloatingButton from "./Components/FloatingButton";
-import GoogleTag from "./Components/GoogleTag";
+// import GoogleTag from "./Components/GoogleTag";
 
 function App() {
   const API_BASE_URL = process.env.REACT_APP_API_URL || "https://api.tellmeyournightmare.com/api/v1/";
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Header />
-      <GoogleTag /> 
+      {/* <GoogleTag />  */}
       <AppRoutes API_BASE_URL={API_BASE_URL} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
       <FloatingButton onClick={() => setIsModalOpen(true)} />
       <ModalForm isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
